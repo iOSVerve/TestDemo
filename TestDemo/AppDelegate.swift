1e6f14346e19c4d8f4dddf90409fbfdec89267b6
iOSVerve <iosvervedeveloper@gmail.com>
2018-02-09 13:50:55 +0530

    na ddd

--- TestDemo/AppDelegate.swift
+++ TestDemo/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+    //Nandoriya
     //Vishal
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
